c21e43e23b1ce2208355e85e2301cc6a8800b398
moonlight169 <monlight169@gmail.com>
2026-04-21 16:08:27 +0700

    robot_m2 walk from robot_m1

--- include/config.h
+++ include/config.h
@@ -1,3 +1,4 @@
+//master
 #define MotorPinFL_A 17
 #define MotorPinFL_B 18
 
@@ -10,7 +11,75 @@
 #define MotorPinRR_A 23
 #define MotorPinRR_B 19
 
-#define MAX_RPM 200
+//master2
+#define MotorPinFL_A 23
+#define MotorPinFL_B 19
+
+#define MotorPinFR_A 16
+#define MotorPinFR_B 4
+
+#define MotorPinRL_A 17
+#define MotorPinRL_B 18
+
+#define MotorPinRR_A 33
+#define MotorPinRR_B 32
+
+// #define MotorPinFL_A 17
+// #define MotorPinFL_B 18
+
+// #define MotorPinFR_A 16
+// #define MotorPinFR_B 4
+
+// #define MotorPinRL_A 33
+// #define MotorPinRL_B 32
+
+// #define MotorPinRR_A 23
+// #define MotorPinRR_B 19
+
+//slave1
+#define MotorPinS1FL_A 17
+#define MotorPinS1FL_B 18
+
+#define MotorPinS1FR_A 16
+#define MotorPinS1FR_B 4
+
+#define MotorPinS1RL_A 33
+#define MotorPinS1RL_B 32
+
+#define MotorPinS1RR_A 23
+#define MotorPinS1RR_B 19
+
+//slave2
+#define MotorPinS2FL_A 17
+#define MotorPinS2FL_B 18
+
+#define MotorPinS2FR_A 16
+#define MotorPinS2FR_B 4
+
+#define MotorPinS2RL_A 33
+#define MotorPinS2RL_B 32
+
+#define MotorPinS2RR_A 23
+#define MotorPinS2RR_B 19
+
+//slave3
+#define MotorPinS3FL_A 17
+#define MotorPinS3FL_B 18
+
+#define MotorPinS3FR_A 16
+#define MotorPinS3FR_B 4
+
+#define MotorPinS3RL_A 33
+#define MotorPinS3RL_B 32
+
+#define MotorPinS3RR_A 23
+#define MotorPinS3RR_B 19
+
+#define M_MAX_RPM 200
+#define M2_MAX_RPM 170
+#define S1_MAX_RPM 200
+#define S2_MAX_RPM 200
+#define S3_MAX_RPM 200
 
 // #define K_P 1.5  // P constant
 // #define K_I 0.25 // I constant
@@ -30,3 +99,8 @@
 
 #define PWM_MAX pow(2, PWM_BITS) - 1
 #define PWM_MIN -PWM_MAX
+
+//master2
+#define WHEEL_DIAMETER_M2 0.090
+#define FR_WHEELS_DISTANCE_M2 0.550
+#define LR_WHEELS_DISTANCE_M2 0.790
---
+++ src/master2.cpp
@@ -0,0 +1,220 @@
+#include "Arduino.h"
+#include "Motor.h"
+#include "config.h"
+#include "Holding.h"
+#include "Kinematics.h"
+#include <PS4Controller.h>
+#include <Wire.h>
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+#define Address_S1 0x01
+#define Address_S2 0x02
+#define Address_S3 0x03
+
+//d4:e9:f4:e2:1c:c8
+
+Motor MotorFL(MotorPinFL_A, MotorPinFL_B, M2_MAX_RPM);
+Motor MotorFR(MotorPinFR_A, MotorPinFR_B, M2_MAX_RPM);
+Motor MotorRL(MotorPinRL_A, MotorPinRL_B, M2_MAX_RPM);
+Motor MotorRR(MotorPinRR_A, MotorPinRR_B, M2_MAX_RPM);
+
+Kinematics kinematics(Kinematics::MECANUM, M2_MAX_RPM, WHEEL_DIAMETER_M2, FR_WHEELS_DISTANCE_M2, LR_WHEELS_DISTANCE_M2);
+
+#define COMMAND_RATE 50
+unsigned long prev_control_time = 0;
+
+float g_req_linear_vel_x = 0;
+float g_req_linear_vel_y = 0;
+float g_req_angular_vel_z = 0;
+
+float f_walkspeed = 1.0;
+float n_walkspeed = 0.5;
+
+float f_turnspeed = 1.5;
+float n_turnspeed = 1.0;
+
+float f_slidespeed = 0.8;
+float n_slidespeed = 0.5;
+//------------------------------------
+float walkspeed = f_walkspeed;
+float turnspeed = f_turnspeed;
+float slidespeed = f_slidespeed;
+
+int speed_mode = 0; 
+
+bool last_options_state = false; 
+bool last_square_state = false;
+bool last_triangle_state = false;
+bool last_circle_state = false;
+
+const int LStickX_Calib = 40;
+const int LStickY_Calib = 20;
+const int RStickX_Calib = 20;
+
+Holding Holding;
+
+void moveBase() {
+  Kinematics::rpm req_rpm = kinematics.getRPM(g_req_linear_vel_x, g_req_linear_vel_y, g_req_angular_vel_z);
+
+  MotorFL.runRPM(req_rpm.motor1);
+  MotorFR.runRPM(req_rpm.motor2);
+  MotorRL.runRPM(req_rpm.motor3);
+  MotorRR.runRPM(req_rpm.motor4);
+}
+
+void update_control() {
+  if (!PS4.isConnected()) {
+    g_req_linear_vel_x = 0; 
+    g_req_linear_vel_y = 0; 
+    g_req_angular_vel_z = 0;
+    MotorFL.run(0); MotorFR.run(0); MotorRL.run(0); MotorRR.run(0);
+    return;
+  }
+
+  bool current_options_state = PS4.Options();
+  if (current_options_state && !last_options_state) {
+    speed_mode = (speed_mode + 1) % 2;
+    if (speed_mode == 0) {
+      walkspeed = f_walkspeed;
+      turnspeed = f_turnspeed;
+      slidespeed = f_slidespeed;
+    } else {
+      walkspeed = n_walkspeed;
+      turnspeed = n_turnspeed;
+      slidespeed = n_slidespeed;
+    }
+  }
+  last_options_state = current_options_state;
+
+  float walk_speed = walkspeed;
+  float turn_speed = turnspeed;
+  float slide_speed = slidespeed;
+
+  float d_x = 0;
+  float d_y = 0;
+  float d_z = 0;
+  float a_x = 0;
+  float a_y = 0;
+  float a_z = 0;
+
+  if (PS4.Up()) {
+    d_x = walk_speed;
+  } else if (PS4.Down()) {
+    d_x = -walk_speed;
+  }
+  
+  if (PS4.Left()) {
+    d_y = slide_speed;
+  } else if (PS4.Right()) {
+    d_y = -slide_speed;
+  }
+
+  if (PS4.L1()) {
+    d_z = turn_speed;  // L1 = หมุนตัวทวนเข็มนาฬิกา
+  } else if (PS4.R1()) {
+    d_z = -turn_speed; // R1 = หมุนตัวตามเข็มนาฬิกา
+  }
+
+  int L_X = PS4.LStickX();
+  int L_Y = PS4.LStickY();
+  int R_X = PS4.RStickX();
+
+  if (abs(L_Y) > LStickY_Calib) {
+    a_x = ((float)L_Y / 127.0) * walk_speed;
+  }
+  if (abs(R_X) > RStickX_Calib) {
+
+    a_y = ((float)R_X / 127.0) * -slide_speed;
+  }
+  if (abs(L_X) > LStickX_Calib) {
+    a_z = ((float)L_X / 127.0) * -turn_speed;
+  }
+
+  g_req_linear_vel_x = a_x + d_x;
+  g_req_linear_vel_y = a_y + d_y;
+  g_req_angular_vel_z = a_z + d_z;
+
+  g_req_linear_vel_x = constrain(g_req_linear_vel_x, -walk_speed, walk_speed);
+  g_req_linear_vel_y = constrain(g_req_linear_vel_y, -slide_speed, slide_speed);
+  g_req_angular_vel_z = constrain(g_req_angular_vel_z, -turn_speed, turn_speed);
+}
+
+void digital_control(){
+  bool square_pressed = PS4.Square();
+  if (square_pressed && !last_square_state) {
+    Wire.beginTransmission(Address_S1);
+    Wire.write("Hello Slave!");   // ส่งข้อมูล
+    Wire.endTransmission();
+    Serial.print("ข้อมูลที่ Slave ตอบกลับมา: ");
+
+    // ร้องขอข้อมูลจำนวน 2 ไบต์ จาก Slave
+    Wire.requestFrom(Address_S1, 2); 
+
+    while (Wire.available()) {
+      char c = Wire.read();
+      Serial.print(c);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
+  }
+  last_square_state = square_pressed;
+
+  bool triangle_pressed = PS4.Triangle();
+  if (triangle_pressed && !last_triangle_state) {
+    Wire.beginTransmission(Address_S2);
+    Wire.write("Hello Slave!");   // ส่งข้อมูล
+    Wire.endTransmission();
+    Serial.print("ข้อมูลที่ Slave ตอบกลับมา: ");
+
+    // ร้องขอข้อมูลจำนวน 2 ไบต์ จาก Slave
+    Wire.requestFrom(Address_S2, 2); 
+
+    while (Wire.available()) {
+      char c = Wire.read();
+      Serial.print(c);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
+  }
+  last_triangle_state = triangle_pressed;
+
+  bool circle_pressed = PS4.Circle();
+  if (circle_pressed && !last_circle_state) {
+    Wire.beginTransmission(Address_S3);
+    Wire.write("Hello Slave!");   // ส่งข้อมูล
+    Wire.endTransmission();
+    Serial.print("ข้อมูลที่ Slave ตอบกลับมา: ");
+
+    // ร้องขอข้อมูลจำนวน 2 ไบต์ จาก Slave
+    Wire.requestFrom(Address_S3, 2); 
+
+    while (Wire.available()) {
+      char c = Wire.read();
+      Serial.print(c);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
+  }
+  last_circle_state = circle_pressed;
+
+}
+
+void setup() {
+  Serial.begin(115200);
+  setCpuFrequencyMhz(240);
+  PS4.begin("d4:e9:f4:e2:1c:c8");
+  Wire.begin(SDA_PIN, SCL_PIN);
+  Holding.init();
+}
+
+void loop() {
+  update_control();
+  digital_control();
+
+  unsigned long now = millis();
+  if ((now - prev_control_time) >= (1000 / COMMAND_RATE)) {
+    moveBase();
+    prev_control_time = now;
+  }
+}
